d067399552dee93e8bfb45409447ab855867ce40
Andoni Rodriguez <andoni_rdgz@outlook.com>
2020-10-01 16:36:48 -0500

    First commit

---
+++ src/main/java/com/mayab/calidad/doubles/tareaUnitTest/Assigment3_UnitTest/App.java
@@ -0,0 +1,13 @@
+package com.mayab.calidad.doubles.tareaUnitTest.Assigment3_UnitTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
