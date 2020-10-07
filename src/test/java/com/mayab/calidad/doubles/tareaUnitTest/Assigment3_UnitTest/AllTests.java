5760f400b8300fc1015eea803c5311aecda2cefa
Andoni Rodriguez <andoni_rdgz@outlook.com>
2020-10-06 19:47:19 -0500

    Se agrega la implementacion de un test suit

---
+++ src/test/java/com/mayab/calidad/doubles/tareaUnitTest/Assigment3_UnitTest/AllTests.java
@@ -0,0 +1,12 @@
+package com.mayab.calidad.doubles.tareaUnitTest.Assigment3_UnitTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestAlerts.class, TestCommisions.class, TestParametrizedAccount.class })
+public class AllTests {
+
+}
+
